b472d8065be871ce19ea9338089567d540ed39ae
Dadepo Aderemi <dadepo@gmail.com>
2020-06-23 21:32:46 +0200

    2 krils, 10 routinators

---
+++ init_krills.py
@@ -0,0 +1,26 @@
+#! /usr/local/bin/python3
+
+
+import argparse
+
+import subprocess
+
+parser = argparse.ArgumentParser()
+
+# script configuration
+
+parser.add_argument("-co", "--count", help="Number of child CA to add")
+parser.add_argument("-rd", "--roadepth", help="Depth to break prefix to create roas for")
+parser.add_argument("-mo", "--mode", help="cli or http", default="cli")
+parser.add_argument("-st", "--sleeptime", help="seconds to sleep between request to prevent overwhelminh", default=2)
+
+args = parser.parse_args()
+
+no_ca = args.count
+roa_depth = int(args.roadepth) - 1
+mode = args.mode
+sleep_time = float(args.sleeptime)
+
+
+subprocess.call(f"docker exec -it krill_1 /usr/bin/python3 data_generator.py --token itworks --host https://localhost:3000 --count {no_ca} --roadepth {roa_depth} --sleeptime {sleep_time} -pr k1", shell=True)
+subprocess.call(f"docker exec -it krill_2 /usr/bin/python3 data_generator.py --token itworks --host https://localhost:3000 --count {no_ca} --roadepth {roa_depth} --sleeptime {sleep_time} -pr k2", shell=True)
