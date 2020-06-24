a7a4e55c0883bee6045b1437aa561416d1f6844f
Dadepo Aderemi <dadepo@gmail.com>
2020-06-24 17:45:44 +0200

    added certificate but still not able to retrieve ta.cer to start

--- rrdp/routinator_img/init_tal.sh
+++ rrdp/routinator_img/init_tal.sh
@@ -3,6 +3,8 @@
 
 wget --no-check-certificate https://192.168.2.101:3000/ta/ta.tal -O /home/routinator/.rpki-cache/tals/krill_unmodified_1.tal
 
+sed -i "1s~.*~https://192.168.2.101:3000/ta/ta.cer~" "/home/routinator/.rpki-cache/tals/krill_unmodified_1.tal"
+
 exec "$@"
 
 
