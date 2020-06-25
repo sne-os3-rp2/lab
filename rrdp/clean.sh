6f79c8efb4789e8a0845d96c013b428ad22b3185
Dadepo Aderemi <dadepo@gmail.com>
2020-06-25 19:50:27 +0200

    Using modified routinator. Still does not work :(

--- rrdp/clean.sh
+++ rrdp/clean.sh
@@ -4,5 +4,6 @@ rm -R ./krill_1_data/cas/*
 rm -R ./krill_1_data/pubd/*
 rm -R ./krill_1_data/repo/*
 rm -R ./krill_1_data/keys/*
-
+rm -R ./krill_1_data/ssl/*
+rm ./docker-compose.yml
 
--- rrdp/routinator_img/init_tal.sh
+++ rrdp/routinator_img/init_tal.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
+sleep 5
 
 wget --no-check-certificate https://192.168.2.101:3000/ta/ta.tal -O /home/routinator/.rpki-cache/tals/krill_unmodified_1.tal
 
