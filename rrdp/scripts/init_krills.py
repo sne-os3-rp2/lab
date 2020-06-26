be986f3d1f3baf418124f6c3e0ba8f0618cdf36f
Dadepo Aderemi <dadepo@gmail.com>
2020-06-26 22:22:56 +0200

    using krill1 in config. extended init_krills

--- rrdp/scripts/init_krills.py
+++ rrdp/scripts/init_krills.py
@@ -13,6 +13,9 @@ parser.add_argument("-c", "--count", help="Number of child CA to add")
 parser.add_argument("-rd", "--roadepth", help="Depth to break prefix to create roas for")
 parser.add_argument("-mo", "--mode", help="cli or http", default="cli")
 parser.add_argument("-st", "--sleeptime", help="seconds to sleep between request to prevent overwhelminh", default=2)
+parser.add_argument("-pr", "--caprefix", help="ca prefix", default="ka")
+
+parser.add_argument("-sr", "--startrange", help="range to start allocating from", default="1.0.0.0/24")
 
 args = parser.parse_args()
 
@@ -20,6 +23,7 @@ no_ca = args.count
 roa_depth = int(args.roadepth) - 1
 mode = args.mode
 sleep_time = float(args.sleeptime)
+ca_prefix = args.caprefix
+start_range = args.startrange
 
-
-subprocess.call(f"docker exec -it krill_unmodified_1 /usr/bin/python3 data_generator.py --token itworks --host https://localhost:3000 --count {no_ca} --roadepth {roa_depth} --sleeptime {sleep_time} -pr k1", shell=True)
+subprocess.call(f"docker exec -it krill_unmodified_1 /usr/bin/python3 data_generator.py --token itworks --host https://localhost:3000 --count {no_ca} --roadepth {roa_depth} --sleeptime {sleep_time} -pr {ca_prefix} -sr {start_range}", shell=True)
